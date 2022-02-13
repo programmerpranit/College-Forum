59d0572daeef624cfb92a6fb70dffc4347046daa
rane_sarvesh <ranesarvesh97@gmail.com>
2022-02-13 21:17:12 +0530

    Question fragment layout is ready...(item_search_question) Search Frag layout is ready...

--- app/src/main/java/com/psp/collegeforum/MainActivity.kt
+++ app/src/main/java/com/psp/collegeforum/MainActivity.kt
@@ -2,10 +2,28 @@ package com.psp.collegeforum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_question.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_main)
+        //Setcolor()
     }
+
+    //FUNCTION TO CHANGE COLOUR OF TEXT OF USER
+    /*
+    fun Setcolor(){
+        var x =3
+        when (x) {
+            1 -> tvName.setTextColor(getResources().getColor(R.color.clr1))
+            2 -> tvName.setTextColor(getResources().getColor(R.color.clr2))
+            3 -> tvName.setTextColor(getResources().getColor(R.color.clr3))
+            4 -> tvName.setTextColor(getResources().getColor(R.color.clr4))
+        }
+
+    }*/
+
 }
